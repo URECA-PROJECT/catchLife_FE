4736d2a5ee382c058bf4a852c72dedbc2b0b9d46
Sanghee Park <tkdgml1241@naver.com>
2024-09-13 17:33:20 +0900

    feat 로그인 회원가입 UI

---
+++ src/pages/user/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignupUser from '../../components/SignupUser';
+import SignupAdmin from '../../components/SignupAdmin';
+
+function Signup(props) {
+    return (
+        <>
+            <h1>회원가입 페이지</h1>
+            <SignupAdmin />
+        </>
+    );
+}
+
+export default Signup;
