a342c0edbdb95b0a91151bd420b381ce8e5c10f6
Sanghee Park <tkdgml1241@naver.com>
2024-09-27 09:56:10 +0900

    fix : 로그인 회원가입 화면 합침

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import Store from "./pages/user/Store";
 import StoreDetail from "./pages/user/StoreDetail";
 import Order from "./pages/user/Order";
 import OrderCustom from "./pages/user/OrderCustom";
-import Signup from "./pages/user/Signup";
 import { RegionProvider } from "./context/RegionContext";
-import SignupUser from "./components/SignupUser";
-import LoginUser from "./components/LoginUser";
-import LoginAdmin from "./components/LoginAdmin";
-import Login from "./pages/user/Login";
-import SignupAdmin1 from "./components/SignupAdmin1";
-import SignupAdmin2 from "./components/SignupAdmin2";
 import MyPage from "./pages/user/MyPage";
 import EditProfile from "./pages/user/EditProfile";
 import ReservationManagement from "./pages/user/ReservationManagement";
@@ -22,6 +15,8 @@ import StoreManagement from "./pages/user/StoreManagement";
 import EditStores from "./pages/user/EditStores";
 import { CategoryProvider } from "./context/CategoryContext";
 import ReservationDetail from "./pages/user/ReservationDetail";
+import LoginPage from "./pages/user/LoginPage";
+import SignupPage from "./pages/user/SignupPage";
 
 function App() {
   return (
@@ -41,15 +36,8 @@ function App() {
               path="/category/:id/:store/:sort/custom"
               element={<OrderCustom />}
             />
-            <Route path="/signup" element={<Signup />} />
-
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signupAdmin1" element={<SignupAdmin1 />} />
-            <Route path="/signupAdmin2" element={<SignupAdmin2 />} />
-            <Route path="/signupUser" element={<SignupUser />} />
-            <Route path="/loginAdmin" element={<LoginAdmin />} />
-            <Route path="/loginUser" element={<LoginUser />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
             <Route path="/mypage" element={<MyPage />} />
             <Route path="/mypage/editprofile" element={<EditProfile />} />
@@ -66,6 +54,7 @@ function App() {
           </Routes>
         </div>
       </CategoryProvider>
+
     </RegionProvider>
   );
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import "../css/Signup.css";
+import Region from "../components/Region";
+
+function Signup() {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        name: '',
+        memberid: '',
+        password: '',
+        phone: '',
+        birth: '',
+        gender: '',
+        address: '',
+        role: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => {
+            if (response.ok) {
+                alert("회원가입이 완료되었습니다.");
+                navigate("/");  // 회원가입 후 메인 페이지로 이동
+            } else {
+                return response.text().then(text => { throw new Error(text) });
+            }
+        })
+        .catch(error => alert(error.message));
+    };
+
+    return (
+        <div className="login-container">
+            <h1 className='signup-h1'>CATCH LIFE</h1>
+            <form className='signup-form' onSubmit={handleSubmit}>
+                <div className='signup-div'>
+                    <input className='signup-input' name="name" type="text" placeholder="이름" value={formData.name} onChange={handleChange} />
+                </div>
+                <div className='signup-div'>
+                    <input className='signup-input' name="memberid" type="text" placeholder="아이디" value={formData.memberid} onChange={handleChange} />
+                </div>
+                <div className='signup-div'>
+                    <input className='signup-input' name="password" type="password" placeholder="비밀번호" value={formData.password} onChange={handleChange} />
+                </div>
+                <div className='signup-div'>
+                    <input className='signup-input' name="phone" type="text" placeholder="전화번호" value={formData.phone} onChange={handleChange} />
+                </div>
+                <div className='signup-div'>
+                    <input className='signup-input' type="date" name="birth" value={formData.birth} onChange={handleChange} />
+                </div>
+                <Region />
+                <div className="signup-signup-div">
+                    <label className='signup-signup-label'>
+                        <input
+                            className='signup-signup-input'
+                            type="radio"
+                            name="role"
+                            value="user"
+                            onChange={handleChange}
+                        /> 고객
+                    </label>
+                    <label className='signup-signup-label' style={{ marginLeft: '20px' }}>
+                        <input
+                            className='signup-signup-input'
+                            type="radio"
+                            name="role"
+                            value="admin"
+                            onChange={handleChange}
+                        /> 관리자
+                    </label>
+                </div>
+                <div className="signup-signup-div">
+                    <label className='signup-signup-label'>
+                        <input
+                            className='signup-signup-input'
+                            type="radio"
+                            name="gender"
+                            value="남"
+                            onChange={handleChange}
+                        /> 남
+                    </label>
+                    <label className='signup-signup-label' style={{ marginLeft: '20px' }}>
+                        <input
+                            className='signup-signup-input'
+                            type="radio"
+                            name="gender"
+                            value="여"
+                            onChange={handleChange}
+                        /> 여
+                    </label>
+                </div>
+                <div className="signup-button-div">
+                    <button type="submit" className="submit-button">회원가입</button>
+                    <button type="button" className="cancel-button" onClick={() => navigate("/")}>
+                        취소
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Signup;
